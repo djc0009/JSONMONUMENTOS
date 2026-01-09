b5b4cecbada37a0dc8b3230a7a4df7f0e10695a0
djc0009 <djc0009@alu.medac.es>
2026-01-09 19:30:44 +0100

    dia 1

---
+++ app/src/main/java/com/example/jsonmonumentos/Monumento.java
@@ -0,0 +1,49 @@
+package com.example.jsonmonumentos;
+
+public class Monumento {
+    private String nombre;
+    private String ubicacion;
+    private String descripcion;
+    private String imagen;
+
+    public Monumento(String nombre, String ubicacion, String descripcion, String imagen) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+}
+
