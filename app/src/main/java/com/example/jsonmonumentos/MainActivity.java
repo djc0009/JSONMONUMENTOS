d27464577f78359a816dd1a04600f08acfa5233b
djc0009 <djc0009@alu.medac.es>
2026-01-14 21:09:50 +0100

    1

--- app/src/main/java/com/example/jsonmonumentos/MainActivity.java
+++ app/src/main/java/com/example/jsonmonumentos/MainActivity.java
@@ -10,9 +10,18 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,11 +69,19 @@ public class MainActivity extends AppCompatActivity {
                             AdaptadorMonumentos adaptadorMonumentos = new AdaptadorMonumentos(MainActivity.this, arrayMonumentos);
                             rvMonumentos.setAdapter(adaptadorMonumentos);
                             adaptadorMonumentos.refrescar();
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Manejar errores de la petición
+                        error.printStackTrace();
+                    }
                 });
-
+        queue.add(jsonArrayRequest);
     }
 }
